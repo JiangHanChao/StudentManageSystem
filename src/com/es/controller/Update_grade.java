725980a4056b2d3190f54c9d49e09c69cdc81157
Jianghanchao <1753786027@qq.com>
2018-12-09 18:57:36 +0800

    add teacher controll

---
+++ src/com/es/controller/Update_grade.java
@@ -0,0 +1,50 @@
+package com.es.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Update_grade
+ */
+@WebServlet("/Update_grade")
+public class Update_grade extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Update_grade() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/500.html").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@SuppressWarnings("static-access")
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+    	response.setCharacterEncoding("utf-8");
+        response.setStatus(response.SC_MOVED_TEMPORARILY);
+    	
+        String sno = request.getParameter("sno");
+		String cno = request.getParameter("cno");
+		String score = request.getParameter("score");
+		
+		System.out.println(sno+" "+cno+" "+score);
+		
+	}
+
+}
